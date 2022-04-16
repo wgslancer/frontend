97b5b2f2150a73c1aca16dced4a7f9178c2363da
wgslancer <wgslancer@gmail.com>
2022-04-17 02:05:26 +0700

    add gsap

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css';
+import { gsap } from 'gsap';
+import { useEffect, useRef } from 'react';
 
 function App() {
-
-  return <div className='App'>
-    This is a test title
-  </div>;
+  const appRef = useRef(null);
+  useEffect(() => {
+    gsap
+      .timeline()
+      .to(appRef.current, {
+        background: 'green',
+      })
+      .to(appRef.current, {
+        background: 'blue',
+      })
+      .repeat(10);
+  }, []);
+  return (
+    <div className='App' style={{ background: 'red' }} ref={appRef}>
+      This is a test title
+    </div>
+  );
 }
 
 export default App;
